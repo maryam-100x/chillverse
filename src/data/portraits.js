eb92667598cd12b1ff2df2d55286eb6c0ecfc259
solpotfun <209683647+solpotfun@users.noreply.github.com>
2025-06-17 18:44:02 +0200

    Updated gallery to 4 per row and other tweaks

--- src/data/portraits.js
+++ src/data/portraits.js
@@ -19,4 +19,40 @@ export const portraits = [
     username: "ChillVerseSOL",
     image: "/portraits/chillverse.png"
   }
+  ,
+  {
+    name: "Hugo",
+    username: "HugoMartingale",
+    image: "/portraits/hugo.png"
+  },
+   {
+    name: "mememem",
+    username: "mememe69696969",
+    image: "/portraits/mememe.png"
+  },
+  {
+    name: "trumpneta",
+    username: "IDF",
+    image: "/portraits/trumpneta.png"
+  },
+  {
+    name: "azrael",
+    username: "onchainjihadi",
+    image: "/portraits/azrael.png"
+  },
+   {
+    name: "ice",
+    username: "ICEgov",
+    image: "/portraits/ice.png"
+  },
+  {
+    name: "orangie",
+    username: "orangie",
+    image: "/portraits/orangie.png"
+  },
+  {
+    name: "shadow",
+    username: "_Shadow36",
+    image: "/portraits/shadow.png"
+  },
 ];
